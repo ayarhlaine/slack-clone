d9a6a59025ba1e045f643c6d81d5923e7c2e4902
Ayar Hlaine <ayarhlaine.dev@gmail.com>
2020-08-11 23:15:40 +0630

    Complete Header (Design)

---
+++ src/Header/Header.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import AccessTimeIcon from '@material-ui/icons/AccessTime';
+import HelpOutlineIcon from '@material-ui/icons/HelpOutline';
+import SearchIcon from '@material-ui/icons/Search';
+import './Header.scss';
+function Header() {
+    return (
+        <div className="Header">
+            <div className="Header__Left">
+                <AccessTimeIcon />
+            </div>
+            <div className="Header__Search">
+                <SearchIcon />
+                <input type="text" value="Search Ayar Hlaine"/>
+            </div>
+            <div className="Header__Right">
+                <HelpOutlineIcon />
+            </div>
+        </div>
+    )
+}
+
+export default Header
