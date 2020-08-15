2500f81bc06e1efb3304094c2b8dd69b3951d92f
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-08-15 05:57:12 +0000

    Deploy ayarhlaine/slack-clone to github.com/ayarhlaine/slack-clone.git:gh-pages

---
+++ precache-manifest.19f5e657d291eadb9f576e1822f6a3b8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6845f9589476d5ae45545bda8e1797e2",
+    "url": "/slack-clone/index.html"
+  },
+  {
+    "revision": "8b13ea4c27455f57d569",
+    "url": "/slack-clone/static/css/main.e3415441.chunk.css"
+  },
+  {
+    "revision": "222f39cc6b0427144185",
+    "url": "/slack-clone/static/js/2.f0e8d7b5.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/slack-clone/static/js/2.f0e8d7b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b13ea4c27455f57d569",
+    "url": "/slack-clone/static/js/main.bf533dab.chunk.js"
+  },
+  {
+    "revision": "f6fc9e51b945821decc8",
+    "url": "/slack-clone/static/js/runtime-main.48a8fb26.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/slack-clone/precache-manifest.19f5e657d291eadb9f576e1822f6a3b8.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/slack-clone/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
